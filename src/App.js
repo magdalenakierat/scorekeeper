fc6671ede29c0f7734a4d280d8669e842d25b5e2
Magdalena Kierat <m.kierat@gmx.de>
2022-06-30 10:51:21 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import Player from "./components/Player/Player.js";
 import { initialPreviousGames } from "./historyDB.js";
 import History from "./components/History/History.js";
 import Form from "./components/Form/Form.js";
+import { Route, Routes } from "react-router-dom";
 
 const players = [
   {
-    name: "John",
+    id: "fjghtueodn12234",
+    name: "Paul",
     score: 15,
   },
   {
-    name: "Jane",
-    score: 15,
+    id: "lfkghgrurncsj305967",
+    name: "Julia",
+    score: 20,
   },
   {
-    name: "Jim",
-    score: 15,
+    id: "05968gjfhrhfn",
+    name: "Ralf",
+    score: 34,
+  },
+  {
+    id: "lgkgj948576",
+    name: "Mareike",
+    score: 10,
   },
 ];
 
@@ -23,19 +32,40 @@ function App() {
   return (
     <>
       <heading>
-        <h1>Game</h1>
+        <h1>Score keeper</h1>
       </heading>
       <main>
-        {players.map((player) => (
-          <Player name={player.name} score={player.score} />
-        ))}
-        <Button>End game</Button>
-        <Form></Form>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <h2>Game</h2>
 
-        <h2>Previous Games</h2>
-        {initialPreviousGames.map((game) => (
-          <History key={game.id} game={game} />
-        ))}
+                {players.map((player) => (
+                  <Player
+                    key={player.id}
+                    name={player.name}
+                    score={player.score}
+                  />
+                ))}
+                <Button>End game</Button>
+                <Form></Form>
+              </>
+            }
+          />
+          <Route
+            path="/history"
+            element={
+              <>
+                <h2>Previous Games</h2>
+                {initialPreviousGames.map((game) => (
+                  <History key={game.id} game={game} />
+                ))}
+              </>
+            }
+          />
+        </Routes>
       </main>
     </>
   );
